29dc065895d7a77208a91a65e69041fdb8c5fc12
Scott <spogatetz8386@stu.d214.org>
2018-03-14 20:46:42 -0500

    Lab

---
+++ Lab 2/ElevensActivityStarterCode/Activity1 Starter Code/CardTester.java
@@ -0,0 +1,19 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+ /**
+  * The main method in this class checks the Card operations for consistency.
+  * @param args is not used.
+  */
+ public static void main(String[] args) {
+  /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+   Card one = new Card("Queen", "Hearts", 11);
+   Card two = new Card("King", "Diamonds", 12);
+   Card three = new Card("Jack", "Spades", 10);
+   System.out.println(one);
+   System.out.println(two);
+   System.out.println(three);
+ }
+}
